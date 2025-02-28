fe0fbc44bf0cb61352ea5b2dfad4e42b7690229f
minhyuk123 <gyo1453@naver.com>
2025-02-28 16:56:28 +0900

    2025.02.28 pmh 공통 setting.py 수정, index.html sidebar, header, footer 분리

--- search/views.py
+++ search/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 # Create your views here.
+
+def search_page(request):
+    posts = Post.objects.all().order_by('-pk')
+    query = request.GET.get('query','')
+    print(query)
+
+
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts' : posts,
+        }
+    )
