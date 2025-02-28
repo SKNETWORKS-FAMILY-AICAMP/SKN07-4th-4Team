bf8dfa5f9ec10cfa6a6786806d37a71d7bfb9c9a
dongik <sdimivy014@gmail.com>
2025-02-28 14:18:44 +0900

    add features in blog: blog list, blog detail, upload files&image, author field

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,36 @@
+import os
+
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    '''
+    장고가 기본적으로 제공하는 render() 함수를 사용행 템플릿 폴더에서 
+    blog 폴더의 index.html 파일을 찾아 방문자에게 보낸다.
+    
+        (1) FBV(Function based view)로 페이지 목록 제작
+    '''
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts' : posts,
+        }
+    )
+
+def post_page(request, pk):
+
+    post =  Post.objects.get(pk=pk)
+    file_name = os.path.basename(post.file_upload.name) 
+
+    return render(
+        request,
+        'blog/post_page.html',
+        {
+            'post' : post,
+            'file_name' : file_name
+        }
+    )
